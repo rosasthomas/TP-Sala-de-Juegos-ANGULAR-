2e245080dfad8f33ee69e4c346b76065b1b17539
rosasthomas <rosasthomas@gmail.com>
2020-04-24 13:18:35 -0300

    arreglo firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -92,14 +92,14 @@ import { AngularFireAuthModule } from 'angularfire2/auth'
   ],
   imports: [
     AngularFireModule.initializeApp({
-      apiKey: "AIzaSyC22XixF38bZawSALGTBTbPVFFNoMAKipc",
-      authDomain: "applogin-bd8cc.firebaseapp.com",
-      databaseURL: "https://applogin-bd8cc.firebaseio.com",
-      projectId: "applogin-bd8cc",
-      storageBucket: "applogin-bd8cc.appspot.com",
-      messagingSenderId: "569152020058",
-      appId: "1:569152020058:web:bd074a788fe0b5ea16c263",
-      measurementId: "G-01RMLCQE7N"
+      apiKey: "AIzaSyChT8970Hbw_H-TVz0z1JeI5OStB2xERFE",
+  authDomain: "sala-de-juegos-11fa2.firebaseapp.com",
+  databaseURL: "https://sala-de-juegos-11fa2.firebaseio.com",
+  projectId: "sala-de-juegos-11fa2",
+  storageBucket: "sala-de-juegos-11fa2.appspot.com",
+  messagingSenderId: "273156643373",
+  appId: "1:273156643373:web:b126b0cfbcc941974ff6a4",
+  measurementId: "G-DLV6VELDVL"
   }),
        AngularFirestoreModule, AngularFireAuthModule,
     BrowserModule,
